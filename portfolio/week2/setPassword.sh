a8b48d5b8b239d9d76db1abb50b50c7a57a3dd34
alice-abernathy <gdacosta@our.ecu.edu.au>
2023-01-24 16:58:43 +0000

    v1.1 hashes password using sha256sum outputs to secret.txt

--- portfolio/week2/setPassword.sh
+++ portfolio/week2/setPassword.sh
@@ -1,9 +1,9 @@
 #2.2
-#Version 1
+#Version 1.1
 #Write a script named 'setPassword.sh' that accomplishes the following:
 # 1 ask the user to type a folder name
 #  2 ask the user to type a secret password (hide that input)
-#   3 save the user’s password in a file called 'secret.txt' inside the folder the user chose.
+#   3 save the user’s password in a file called 'secret.txt' as a hash value using sha256sum
 #
 # 1 ask the user to type a folder name
 read -p "Please Enter Folder Name: " dirname
@@ -15,8 +15,9 @@ cd $dirname
 echo -n "Please Enter Your Password: "
 read -s password
 #
-#   3 save the user’s password in a file called 'secret.txt' inside the folder the user chose.
-echo $password > secret.txt
+#   3 save the user’s password in a file called 'secret.txt' as a hash value using sha256sum
+echo $password | sha256sum > secret.txt
 echo
 #
 exit
+
