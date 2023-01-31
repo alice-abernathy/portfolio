b2d3cda29766d3c8cc3e5d7331c8ad26e64588f4
alice-abernathy <gdacosta@our.ecu.edu.au>
2023-01-31 11:06:09 +0000

    Add Week 2 scripts to portfolio

--- portfolio/week2/setPassword.sh
+++ portfolio/week2/setPassword.sh
@@ -5,13 +5,13 @@
 # 1 ask the user to type a folder name
 #  2 ask the user to type a secret password (hide that input)
 #   3 save the user’s password in a file called 'secret.txt' as a hash value using sha256sum
-#
+
 # 1 ask the user to type a folder name
 read -p "Please Enter Folder Name: " dirname
 mkdir $dirname
 echo "Folder $dirname Created"
 cd $dirname
-#
+
 #  2 ask the user to type a secret password (hide that input)
 echo -n "Please Enter Your Password: "
 read -s password
