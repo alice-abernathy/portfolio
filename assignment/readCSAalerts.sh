7d25711fd51127b9d9d5cdd13fa136dc8659b192
“alice-abernathy” <gdacosta@our.ecu.edu.au>
2023-02-16 12:40:03 +0000

    adds color, minor changes to text output

--- assignment/readCSAalerts.sh
+++ assignment/readCSAalerts.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+#v2.0 
+
+#Define colors
+BLK="\033[30m"
+RED="\033[31m"
+BRED="\033[1;31m"
+GRN="\033[32m"
+BGRN="\033[1;32m"
+BRN="\033[33m"
+BBRN="\033[1;33m"
+BLU="\033[34m"
+BBLU="\033[1;34m"
+PRPL="\033[35m"
+BPRPL="\033[1;35m"
+CYN="\033[36m"
+BCYN="\033[1;36m"
+GRY="\033[37m"
+YLY="\033[0;33m"
+BYLY="\033[1;33m"
+BMAG="\033[1;35m"
+
+#no color
+RST="\033[0m"
 
 #set the input file name
 
@@ -9,6 +32,11 @@ input_file="CSAalerts.txt"
 html_tags=("<h3 class=\"sc-card-title\">" "<div class=\"sc-card-publish\">" "<p class=\"sc-card-desc\">")
 
 #Loop through each line of the input file
+
+echo
+echo -e "${BYLY}Below is timely information about security issues, vulnerabilities, and exploits"
+echo -e "Provided by ${RST}${BMAG}Singapore Computer Emergency Response Team${RST}"
+echo
 
 while read line; do
 
@@ -23,8 +51,12 @@ while read line; do
             #If there's a match, remove the HTML tag and print the line
 
             line_without_tag=$(echo "$line" | sed "s/$tag//g" | sed "s/<\/${tag:1}/\n/g" | sed "s/<[^>]*>//g")
-            echo "$line_without_tag"
+            echo -e "${YLY} $line_without_tag ${RST}"
+            echo
             break
         fi
     done
+
+
+
 done < "$input_file"
