fc8adde49654c7e13b0dcee5d41419d3c437680c
“alice-abernathy” <gdacosta@our.ecu.edu.au>
2023-02-18 17:05:03 +0000

    Final working version with awk filter and some colour

--- portfolio/week5/UserAccounts.sh
+++ portfolio/week5/UserAccounts.sh
@@ -1,32 +1,62 @@
-#!/bin/bash
-#V1.1
-#increase column widths, V1 showed data can be very long
-#add table
+#/bin/bash
+#V2.2 
+
+#5.5
+#Now write a script named “UserAccounts.sh” to take a look at the “/etc/passwd” file and format it into a neat table similar to the one from task 5.4
+#Use Regex to make the  “UserAccounts.sh” awk script filter the data to only show users who have “/bin/bash” listed as their default shell.
+
+
+#awk filter included
+#headings have colour
+#to add more colour will need alot of work as escape codes to add colour wrecks the column widths
 
 #set column widths
+
 columnwidth_1=20
 columnwidth_2=10
-columnwidth_3=10
-columnwidth_4=30
+columnwidth_3=12
+columnwidth_4=40
 columnwidth_5=30
 
- #print table headings
+tableLINE() 
+{
+  n=110
+  printf '%*s\n' $n | tr ' ' '_'
+}
 
- printf "%-${columnwidth_1}s" "username"
- printf "%-${columnwidth_2}s" "user id"
- printf "%-${columnwidth_3}s" "group id"
- printf "%-${columnwidth_4}s" "home"
- printf "%-${columnwidth_5}s\n" "shell"
+#print table top line
 
- #use awk to format table rows
- #
+echo ""
+echo ""
+tableLINE
 
- awk -F: -v w1="$columnwidth_1" -v w2="$columnwidth_2" -v w3="$columnwidth_3" -v w4="$columnwidth_4" -v w5="$columnwidth_5" '{
+# print table headings
 
-     printf("%-*s", w1, $1);
-     printf("%-*s", w2, $3);
-     printf("%-*s", w3, $4);
-     printf("%-*s", w4, $6);
-     printf("%-*s\n", w5, $7);
+printf "%-${columnwidth_1}s" "| $(tput setaf 4)Username$(tput sgr0)          "
+printf "%-${columnwidth_2}s" "| $(tput setaf 4)User ID$(tput sgr0) "
+printf "%-${columnwidth_3}s" "| $(tput setaf 4)Group ID$(tput sgr0)  "
+printf "%-${columnwidth_4}s" "| $(tput setaf 4)Home$(tput sgr0)                                  "
+printf "%-${columnwidth_5}s\n" "| $(tput setaf 4)Shell$(tput sgr0)                    |"
 
-}' inputfile.txt
+#print table separator
+
+tableLINE
+
+#use awk to format table rows
+awk -F: -v w1="$columnwidth_1" -v w2="$columnwidth_2" -v w3="$columnwidth_3" -v w4="$columnwidth_4" -v w5="$columnwidth_5" '
+
+$7 == "/bin/bash" {
+
+     printf("%-*s", w1, "| " $1);
+     printf("%-*s", w2, "| " $3);
+     printf("%-*s", w3, "| " $4);
+     printf("%-*s", w4, "| " $6);
+     printf("%-*s\n", w5, "| "$7 "                |");
+
+}' inputfile.txt 
+
+# print table footer
+
+tableLINE
+echo ""
+echo ""
