0ad414fb8cd2a7b1349803dc646167b9380bae1c
“alice-abernathy” <gdacosta@our.ecu.edu.au>
2023-02-20 13:30:02 +0000

    added color

--- portfolio/week1/second.sh
+++ portfolio/week1/second.sh
@@ -1,8 +1,47 @@
 #!/bin/bash
+#1.4
 
-echo "Hi there!"
 
-echo "It's good to see you $1!"
+#define colors
+BLK="\033[30m"
+
+RED="\033[31m"
+
+BRED="\033[1;31m"
+
+GRN="\033[32m"
+
+BGRN="\033[1;32m"
+
+BRN="\033[33m"
+
+BBRN="\033[1;33m"
+
+BLU="\033[34m"
+
+BBLU="\033[1;34m"
+
+PRPL="\033[35m"
+
+BPRPL="\033[1;35m"
+
+CYN="\033[36m"
+
+BCYN="\033[1;36m"
+
+GRY="\033[37m"
+
+YLY="\033[0;33m"
+
+BYLY="\033[1;33m"
+
+#no color
+RST="\033[0m"
+
+
+echo -e "${BBLU}Hi there!"
+
+echo -e "It's good to see you ${RST}${BPRPL}$1${BBLU} !!!!${RST}"
 
 exit 0
 					
