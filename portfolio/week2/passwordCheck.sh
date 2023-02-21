f901efed78f14c1b468a1c06ae689a6189d056ca
“alice-abernathy” <gdacosta@our.ecu.edu.au>
2023-02-21 13:47:51 +0000

    Add color. Small changes how the output looks

--- portfolio/week2/passwordCheck.sh
+++ portfolio/week2/passwordCheck.sh
@@ -5,13 +5,50 @@
 # If the user’s password is correct, print “Access Granted” and quit with the exit code 0.
 # If the user’s password is incorrect print “Access Denied” and quit with the exit code 1.
 
+#Define colors
+BLK="\033[30m"
 
-echo -n "Please type your password: "
-read -s password
+RED="\033[31m"
+
+BRED="\033[1;31m"
+
+GRN="\033[32m"
+
+BGRN="\033[1;32m"
+
+BRN="\033[33m"
+
+BBRN="\033[1;33m"
+
+BLU="\033[34m"
+
+BBLU="\033[1;34m"
+
+PRPL="\033[35m"
+
+BPRPL="\033[1;35m"
+
+CYN="\033[36m"
+
+BCYN="\033[1;36m"
+
+GRY="\033[37m"
+
+YLY="\033[0;33m"
+
+BYLY="\033[1;33m"
+
+#no color
+RST="\033[0m"
+
+echo ""
+echo -n -e "${BPRPL}Please type your password: ${RST}"
+read -s -r password
+echo ""
 
 #store entered password to input.txt
 
-echo $password | sha256sum > input.txt
+echo "$password" | sha256sum > input.txt
 
 #compare the hashed value in input.txt and secret.txt
 
@@ -19,21 +56,23 @@ if [ "$(cat input.txt)" == "$(cat secret.txt)" ];
 
 then 
 
-    echo
-    echo "Access Granted"
+    echo ""
+    echo -e "${BGRN}Access Granted${RST}"
+    echo ""
+
     #delete input.txt file
     rm input.txt
     exit 0
 
 else
 
-    echo
-    echo "Access Denied"
+    echo ""
+    echo -e "${BRED}Access Denied${RST}"
+    echo ""
     #delete input.txt file
     rm input.txt
     exit 1
 
 fi
 
-echo
 
